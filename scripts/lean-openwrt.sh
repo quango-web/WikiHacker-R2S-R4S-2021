8370d601de0e174f3a64980821599048282613a5
wikihacker <naveed7us@gmail.com>
2021-03-04 12:44:51 +0800

    fix

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -58,7 +58,9 @@ git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add luci-app-gowebdav
-git clone --depth=1 https://github.com/project-openwrt/openwrt-gowebdav
+#git clone --depth=1 https://github.com/project-openwrt/openwrt-gowebdav
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/gowebdav
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-gowebdav
 
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon

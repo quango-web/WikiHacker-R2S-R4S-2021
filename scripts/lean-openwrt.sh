4243e059d6fa75b2beda65f2cda81a3b9a899341
wikihacker <naveed7us@gmail.com>
2021-03-10 13:32:48 +0800

    added ddns source

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -142,3 +142,16 @@ popd
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+#DDNS
+rm -rf ./feeds/packages/net/ddns-scripts
+rm -rf ./feeds/luci/applications/luci-app-ddns
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun package/lean/ddns-scripts_aliyun
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_dnspod package/lean/ddns-scripts_dnspod
+svn co https://github.com/openwrt/packages/branches/openwrt-18.06/net/ddns-scripts feeds/packages/net/ddns-scripts
+svn co https://github.com/openwrt/luci/branches/openwrt-18.06/applications/luci-app-ddns feeds/luci/applications/luci-app-ddns
+
+#腾讯DDNS
+svn co https://github.com/QiuSimons/OpenWrt_luci-app/trunk/others/luci-app-tencentddns package/lean/luci-app-tencentddns
+#阿里DDNS
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-aliddns package/new/luci-app-aliddns
